72b093d90482b800577e4d36fd54b5eb73190b47
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-13 22:12:32 +0200

    skull

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,27 +1,28 @@
-import { errorResponse, getBodyAsync, getParams } from "@/utils/request";
+import { errorResponse, getBodyAsync } from "@/utils/request";
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import { StatusCodes } from "http-status-codes";
 import { db } from "@/utils/db";
 import { requireAuth } from "@/utils/request";
-import { z } from "zod";
+import { UserModel } from "@/prisma/zod";
+import { User } from "@prisma/client";
 
 export async function GET(req: NextRequest, route: { params: { id: string } }) {
   try {
-    // if (await getToken({ req })) {
-    let user;
-    let id = Number(route.params.id);
-    if (id) {
-      user = await db.user.findUnique({
-        where: { id: id },
+    if (await getToken({ req })) {
+      let user: User | null = null;
+      let id = Number(route.params.id);
+      if (id) {
+        user = await db.user.findUnique({
+          where: { id: id },
+        });
+      }
+      return NextResponse.json(user || { error: true, message: "Not found" }, {
+        status: StatusCodes.BAD_REQUEST,
       });
+    } else {
+      return requireAuth(req);
     }
-    return NextResponse.json(user || { error: true, message: "Not found" }, {
-      status: StatusCodes.BAD_REQUEST,
-    });
-    // } else {
-    //   return requireAuth(req);
-    // }
   } catch (err) {
     return errorResponse(err as Error);
   }
@@ -33,26 +34,20 @@ export async function PATCH(
 ) {
   try {
     if (await getToken({ req })) {
-      const id: number = Number(route.params.id);
-      const body = await getBodyAsync(req);
-
-      return NextResponse.json({});
+      let user: User | null = null;
+      let id = Number(route.params.id);
+      if (id) {
+        const body = UserModel.partial().parse(await getBodyAsync(req));
+        user = await db.user.update({ where: { id: id }, data: body });
+        return NextResponse.json(
+          user || { error: true, message: "Not found" },
+          {
+            status: StatusCodes.BAD_REQUEST,
+          }
+        );
+      }
     } else {
       return requireAuth(req);
-    }
-  } catch (err) {
-    return errorResponse(err as Error);
-  }
-}
-
-export async function PUT(req: NextRequest, route: { params: { id: string } }) {
-  try {
-    const jwtToken = await getToken({ req });
-    if (jwtToken) {
-      const id: number = Number(route.params.id);
-      const body = await getBodyAsync(req);
-
-      return NextResponse.json({}, { status: StatusCodes.CREATED });
     }
   } catch (err) {
     return errorResponse(err as Error);
@@ -64,18 +59,20 @@ export async function DELETE(
   route: { params: { id: string } }
 ) {
   try {
-    const jwtToken = await getToken({ req });
-    if (jwtToken) {
-      const id: number = Number(route.params.id);
-      const body = await getBodyAsync(req);
-
-      return NextResponse.json(
-        {
-          error: false,
-          message: "",
-        },
-        { status: StatusCodes.NO_CONTENT }
-      );
+    if (await getToken({ req })) {
+      let user: User | null = null;
+      let id = Number(route.params.id);
+      if (id) {
+        user = await db.user.delete({ where: { id: id } });
+        return NextResponse.json(
+          user || { error: true, message: "Not found" },
+          {
+            status: StatusCodes.BAD_REQUEST,
+          }
+        );
+      }
+    } else {
+      return requireAuth(req);
     }
   } catch (err) {
     return errorResponse(err as Error);
