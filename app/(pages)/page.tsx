1304be33a3944253b4755f2ed5f7779f5f311cd5
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2024-10-20 20:16:14 +0200

    wip

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,16 +1,12 @@
-import Link from "next/link";
-import Image from "next/image";
 import { ArrowDownToLine } from "lucide-react";
-
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function HomePage() {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 relative">
       <header className="px-4 lg:px-6 h-14 flex items-center">
-        <Link
-          className="flex items-center justify-center no-underline gap-4"
-          href="#"
-        >
+        <Link className="flex items-center justify-center no-underline gap-4" href="#">
           <Image src="/images/icon.svg" alt="Mia" width={32} height={32} />
           Mia
         </Link>
@@ -19,7 +15,7 @@ export default async function HomePage() {
         {/* Add download button and arrow */}
         <div className="flex justify-center 2xl:absolute top-24 right-6  ">
           <a
-            href="https://drive.usercontent.google.com/download?id=1rvR7Ww7RcR7ESWAO7rnFcCvx4zHj6eGb&export=download&authuser=0&confirm=t&uuid=cea8f42e-6c6f-42ba-9134-c84eef0d1c10&at=AN_67v3plsqABEykUYie3cjVPSX8%3A1728922027854"
+            href="https://drive.usercontent.google.com/download?id=1LnuB5YQLDqBsQ3FLrQIcvBLW4xSSgWfV&export=download&authuser=0&confirm=t&uuid=cea8f42e-6c6f-42ba-9134-c84eef0d1c10&at=AN_67v3plsqABEykUYie3cjVPSX8%3A1728922027854"
             className="no-underline inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             target="_blank"
             rel="noopener noreferrer"
@@ -30,7 +26,6 @@ export default async function HomePage() {
           </a>
         </div>
         <Image
-
           src="/images/arrow.svg"
           alt="Arrow pointing to download"
           width={100}
@@ -51,9 +46,10 @@ export default async function HomePage() {
                   </p>
                   <p>
                     Mia <strong>runs on your device</strong>, ensuring <strong>privacy</strong> and fast performance.
-
                   </p>
-                  <p>It <strong>can browse</strong> and <strong>learn</strong> from the web.</p>
+                  <p>
+                    It <strong>can browse</strong> and <strong>learn</strong> from the web.
+                  </p>
                   <p>
                     Call it from <strong>anywhere</strong>, anytime (⌘+p)
                   </p>
@@ -62,7 +58,13 @@ export default async function HomePage() {
 
               {/* Add video component */}
               <div className="mt-8 flex justify-center rounded-lg w-fit">
-                <iframe style={{ borderRadius: "12px" }} src="https://drive.google.com/file/d/1GuQgAfhZ-QQHCxextHilTTaEPrr3KZ7C/preview" width="640" height="480" allow="autoplay"></iframe>
+                <iframe
+                  style={{ borderRadius: "12px" }}
+                  src="https://drive.google.com/file/d/1GuQgAfhZ-QQHCxextHilTTaEPrr3KZ7C/preview"
+                  width="640"
+                  height="480"
+                  allow="autoplay"
+                ></iframe>
               </div>
             </div>
           </div>
